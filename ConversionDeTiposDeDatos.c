d7fff7930bdeb7974853d891ae5830eb13266ae5
Marco <maphy1707@gmail.com>
2023-07-24 22:25:04 -0600

    Convercion de tipos de datos

---
+++ ConversionDeTiposDeDatos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a = 16.20;
+	
+	printf(" Entero:%i\n Flotante:%.2f\n Double:%.3f\n Caracter:%c\n", a, (float)a, (double)a, (char)a);
+	
+	return 0;
+}
